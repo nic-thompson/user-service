6205bf00ecddc7f1447f92708465f70fa763bbf3
Nic Thompson <nic.thompson@vocovo.com>
2024-07-14 13:47:16 +0100

    fix: userId type

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,9 @@
-export const fetchUserData = async (userId: string): Promise<{ id: number, name: string, email: string }> => {
-    const response = await fetch(`https://nicolasthompson.com/api/users/${userId}`);
-    const data = await response.json();
-    return data;
-}
+export const fetchUserData = async (
+  userId: number
+): Promise<{ id: number; name: string; email: string }> => {
+  const response = await fetch(
+    `https://nicolasthompson.com/api/users/${userId}`
+  );
+  const data = await response.json();
+  return data;
+};
